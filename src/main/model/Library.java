886c2c6a03d59ac447974b050b572e1b4e876bc0
Julien LaPointe <email@julienlapointe.com>
2020-10-29 11:41:43 -0400

    Added data persistence user stories to README.md

--- src/main/model/Library.java
+++ src/main/model/Library.java
@@ -1,9 +1,6 @@
 package model;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
 
 // Represents a library of all quotes
 public class Library {
@@ -26,11 +23,6 @@ public class Library {
         }
         quotes.add(newQuote);
         return true;
-//        if (getAllQuotes().contains(newQuote.phrase)) {
-//            return false;
-//        }
-//        quotes.add(newQuote);
-//        return true;
     }
 
     // REQUIRES: quote contains a phrase String of non-zero length
@@ -39,25 +31,6 @@ public class Library {
     public void removeQuote(Quote quote) {
         quotes.remove(quote);
     }
-
-    // REQUIRES: quote contains a phrase String of non-zero length
-    // MODIFIES: this
-    // EFFECTS: New quote replaces old quote in Library of Quotes
-//    public void editQuote(Quote editedQuote) {
-//        for (Quote quote : quotes) {
-//           if (Objects.equals(quote.getId(), editedQuote.getId())) {
-//               quote = editedQuote;
-//           }
-//        }
-//    }
-
-    // REQUIRES: 1 <= index <= total number of Quotes in Library
-    // EFFECTS: returns the Quote where ID matches index - 1
-//    public Quote getQuote(int index) {
-//        Quote quote;
-//        quote = quotes.get(index - 1);
-//        return quote;
-//    }
 
     // EFFECTS: returns an ArrayList of all Quotes in Library
     public ArrayList<Quote> getAllQuotes() {
--- src/test/model/LibraryTest.java
+++ src/test/model/LibraryTest.java
@@ -10,17 +10,17 @@ class LibraryTest {
 
     private Library testLibrary;
     private Quote quote1;
-    private Quote quote2;
+//    private Quote quote2;
     private String phrase1 = "Phrase 1";
     private String author1 = "Author 1";
-    private String phrase2 = "Phrase 2";
-    private String author2 = "Author 2";
+//    private String phrase2 = "Phrase 2";
+//    private String author2 = "Author 2";
 
     @BeforeEach
     void setup() {
         testLibrary = new Library();
         quote1 = new Quote(phrase1, author1);
-        quote2 = new Quote(phrase2, author2);
+//        quote2 = new Quote(phrase2, author2);
     }
 
     // Source: IntegerSet example
