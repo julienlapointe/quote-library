10dcf32a101fbfd6c3b6320f0f05aa09d78e6496
Julien LaPointe <email@julienlapointe.com>
2020-11-25 21:32:59 -0500

    Updated comments in Main.java.

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -11,8 +11,27 @@
 // - Should "robust" code ideally have no REQUIRES clauses?
 // - Where do cite / credit other code? Ex. Persistence code
 // - Are we allowed to use a Swing library for UI?
-// PHASE 3
-// -
+// PHASE 3 / 4
+// - Are event listener and handler the same thing in Swing?
+// - What is the purpose of mirroring UI list items in the model then converting to JSON
+//   when I could just grab the values from UI and convert them to JSON?
+
+// TODO:
+// - Make rest of GUI event listeners / handlers update the model (addListener is done)
+// - Check that DuplicateException is NOT thrown when an item is added, removed, then re-added
+// - Check that DuplicateException is thrown when duplicate quote with empty author ("Anonymous")
+//   is added to list and phrase already exists in list
+// - Add empty author ("Anonymous") feature to Edit dialog
+// - Fix textField focus issue after a DuplicateException / EmptyException
+// - Pop-up dialog with error message for DuplicateException / EmptyException
+// - Add drag-and-drop functionality to list (must update the model)
+// - Add "Quote Show" carousel visual display of quotes (random order)
+// - Add tags to Quotes using HashMap
+// - Filter quotes by tag
+// - Search
+// - Refactor GUI.java to (1) have a sound() helper method and (2) put each of the five
+//   event listeners / handlers into their own files
+// - Comments / Credits
 
 package ui;
 
